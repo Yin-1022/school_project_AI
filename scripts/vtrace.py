4c3f69ac83236547cecc2732026c1e7c86d89426
Yin-1022 <kv.huang931022@gmail.com>
2026-08-23 15:44:48 +0800

    reward update_11-1: Importance Sampling Ratio

--- scripts/vtrace.py
+++ scripts/vtrace.py
@@ -3,7 +3,7 @@ import torch
 def compute_importance_ratios(target_logits, actions, behavior_log_prob,):
     target_log_prob = torch.log_softmax(target_logits, dim=-1)
     target_action_log_prob = torch.gather(
-            target_logits, dim=-1, index=actions.unsqueeze(-1)
+            target_log_prob, dim=-1, index=actions.unsqueeze(-1)
         ).squeeze(-1)
 
     #Ratio between Actor收集資料時的policy & 當前Learner對此action之看法
